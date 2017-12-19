35c0084f4bfd90d2a2cc627b464d3850a828b3f8
nico-develop <contact@nico-develop.fr>
2017-12-19 13:55:28 +0100

    Ajout du tri à bulle

---
+++ customSort.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$array = [10, 3, 7, 5, 1];
+
+$sorted = false;
+
+$size = count($array);
+
+while(!$sorted)
+{
+    $sorted = true;
+
+    for($i = 0 ; $i < $size - 1 ; $i++)
+    {
+        if($array[$i] > $array[$i + 1])
+        {
+            $tmp = $array[$i];
+
+            $array[$i] = $array[$i + 1];
+            $array[$i + 1] = $tmp;
+
+            $sorted = false;
+        }
+    }
+
+    $size--;
+}
+
+var_dump($array);
+
