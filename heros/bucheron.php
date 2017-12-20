54bb8cc5321ec7e37eb76359bd58f1f9f88c5322
Qleroty <quentinleroty@gmail.com>
2017-12-20 13:39:35 +0100

    dz

---
+++ heros/bucheron.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class Bucheron extends Personnage {
+  private $role;
+  private $pv;
+  private $arme;
+
+
+  public function display($name = null):array {
+      $caract = parent::display();
+      $caract["name"] = $name;
+      return $caract;
+
+
+public function getrole() {
+  return $this->role;
+}
+public function setrole() {
+  $this->role = $role;
+}
+
+public function getpv() {
+  return $this->pv;
+}
+public function setpv() {
+  $this->pv = $pv;
+}
+
+public function getarme() {
+  return $this->arme;
+}
+public function setarme() {
+  $this->arme = $arme;
+}
+
+public function display():string {
+      return "le" . $this->role . "possède une " . $this->arme .".";
+}
+}
+
+
+ ?>
