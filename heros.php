6273d70474c3bf7f8badb599a1deb3195c4235f0
Qleroty <quentinleroty@gmail.com>
2017-12-20 13:36:43 +0100

    RepoQLeroty

---
+++ heros.php
@@ -0,0 +1,30 @@
+<?php
+
+class Personnage {
+  private $name;
+  private $pv;
+  private $arme;
+
+
+public function getname() {
+  return $this->name;
+}
+public function setname() {
+  $this->name = $name;
+}
+
+public function getpv() {
+  return $this->pv;
+}
+public function setpv() {
+  $this->pv = $pv;
+}
+
+public function display():string {
+      return "ce héros est" . $this->name . "et possède " . $this->pv . "points de vie";
+}
+
+
+}
+
+ ?>
